1deef73bc077cefd8f56066de7d5bee12ca6f5db
Praveen O Krishnan <praveen.k84@wipro.com>
2018-06-01 17:56:11 +0530

    angular code

---
+++ components/classifieds.ctr.js
@@ -0,0 +1,24 @@
+(function() {
+  "use strict";
+
+  angular
+    .module("ngClassiFieds")
+    .controller("classifiedsCtrl", function(
+      $scope,
+      $http,
+      classifiedsFactory,
+      $mdSidenav
+    ) {
+      classifiedsFactory.getClassifieds().then(function(response) {
+        $scope.classifieds = response.data;
+      });
+
+      $scope.openSidebar = function() {
+        $mdSidenav("left").open();
+      };
+
+      $scope.closeSidebar = function() {
+        $mdSidenav("left").close();
+      };
+    });
+})();
---
+++ scripts/app.js
@@ -0,0 +1,13 @@
+angular
+  .module("ngClassiFieds", ["ngMaterial"])
+  .config(function($mdThemingProvider) {
+    $mdThemingProvider
+      .theme("default")
+      .primaryPalette("teal")
+      .accentPalette("orange");
+  })
+  .directive("helloWorld", function() {
+    return {
+      template: "<h1>{{ message }}</h1>"
+    };
+  });
